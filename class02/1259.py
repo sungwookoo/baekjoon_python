2a63b5bab3e5fc909b91a76e278833fa5076e706
sungwookoo <sungwookoo.dev@gmail.com>
2022-05-11 23:41:41 +0900

    Solved 팰린드롬수

---
+++ class02/1259.py
@@ -0,0 +1,17 @@
+﻿# 팰린드롬수 : 거꾸로 읽어도 같은 수
+input_list = []
+result_list = []
+while(True):
+    num = input()
+    if num == '0':
+        break
+    input_list.append(num)
+
+for i in input_list:
+    back = i[-(len(i)//2):]
+    front = i[(len(i)//2)-1::-1]
+    result_list.append('yes') if back == front else result_list.append('no')
+
+
+for i in result_list:
+    print(i)
