2e3a01fb973d76783873d3f41357eaa3c3e2869c
sungwookoo <sungwookoo.dev@gmail.com>
2022-05-25 17:20:05 +0900

    Solved 카드2

---
+++ class02/2164.py
@@ -0,0 +1,18 @@
+﻿# 카드2
+from collections import deque
+from sys import stdin
+n = int(stdin.readline())
+que = deque()
+for i in range(n):
+    que.append(i+1)
+    # 1 2 3 4 5 6
+    # 3 4 5 6 2
+    # 5 6 2 4
+    # 2 4 6
+    # 6 4
+    # 4
+for _ in range(len(que)-1):
+    que.popleft()
+    que.append(que.popleft())
+
+print(que.popleft())
