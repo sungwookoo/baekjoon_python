6f7e82cda7b8e296c07fb13bf62df776a093b526
sungwookoo <sungwookoo.dev@gmail.com>
2022-05-25 17:30:05 +0900

    Solved 수 정렬하기 2

---
+++ class02/2751.py
@@ -0,0 +1,10 @@
+﻿# 수 정렬하기 2
+from sys import stdin
+n = int(stdin.readline())
+input_list = [i for i in range(10000)]
+for _ in range(n):
+    input_list.append(int(stdin.readline()))
+
+input_list.sort()
+for i in input_list:
+    print(i)
