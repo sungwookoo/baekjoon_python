d539000108aa25b265aa219dc62fb50067e1a818
sungwookoo <sungwookoo.dev@gmail.com>
2022-05-26 09:04:47 +0900

    Solved 블랙잭

---
+++ class02/2798.py
@@ -0,0 +1,15 @@
+﻿# 블랙잭
+from sys import stdin
+n, m = map(int, stdin.readline().split())
+cards = list(map(int, stdin.readline().split()))
+sum = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if cards[i] + cards[j] + cards[k] > m:
+                continue
+            else:
+                sum = max(sum, cards[i]+cards[j]+cards[k])
+
+print(sum)
